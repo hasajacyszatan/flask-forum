166a3a0965eb3afe016c98c258f078a17462eb86
Kamil <62152703+hasajacyszatan@users.noreply.github.com>
2021-07-15 22:49:12 +0200

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template, request, redirect, url_for
-app = Flask(__name__)
-@app.route('/', methods=['POST'])
-def upload_file():
-    uploaded_file = request.files['file']
-    if uploaded_file.filename != '':
-        uploaded_file.save(uploaded_file.filename)
-    return redirect(url_for('index'))
-app.run(host="0.0.0.0")
